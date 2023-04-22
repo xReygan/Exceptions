12ef1df203474d3ff8f82bbc1d82e300624da7c5
AndreyGurinovich <rey74@bk.ru>
2023-04-22 12:38:54 +0500

    Семинар 1 Задание 1

---
+++ seminar1/task1.java
@@ -0,0 +1,31 @@
+//Реализуйте 3 метода, чтобы в каждом из них получить разные исключения
+
+package seminar1;
+
+public class task1 {
+    public static void main(String[] args) {
+        String []s = {"one", "two", null};
+        int[] array = {1,2,3,4,5,6,7,8,9,10};
+        divide(10, 1);
+        printMeaning(array,1);
+        printStringLength(s);
+    }
+
+    public static void divide(int num1, int num2){
+        if(num2 == 0) throw new RuntimeException("На ноль делить нельзя!!!");
+        double res = num1/num2;
+        System.out.println("если " + num1 + " разделить на " + num2 + " получаем " + res);
+    }
+
+    public static void printMeaning(int[] array, int index){
+        if(index > array.length-1) throw new RuntimeException("Элемента с таким индексом не существует");
+        System.out.println("элемент индекса " + index + " равен " + array[index]);
+    }
+
+    public static void printStringLength(String[] s){
+        for (String x: s){
+            if(x == null) throw new RuntimeException("Элемент отсутствует!!!");
+            System.out.println(x);
+        }
+    }
+}
