dc8918ad95f4b03c1b941252843ba60149ff1054
AndreyGurinovich <rey74@bk.ru>
2023-04-23 13:52:43 +0500

    Семинар 2 Залание 1, 2, 3

--- seminar2/task2.java
+++ seminar2/task2.java
@@ -1,15 +1,25 @@
+// Если необходимо, исправьте данный код
+/*
+try {
+    int d = 0;
+    double catchedRes1 = intArray[8] / d;
+    System.out.println("catchedRes1 = " + catchedRes1);
+} catch (ArithmeticException e) {
+    System.out.println("Catching exception: " + e);
+}
+ */
+
 package seminar2;
 
 public class task2 {
     public static void main(String[] args) {
         try {
             int d = 0;
-            int[] intArray = {1, 5, 6, 8, 7, 9, 2, 1, 9};
+            int[] intArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
             double catchedRes1 = intArray[8] / d;
             System.out.println("catchedRes1 = " + catchedRes1);
         } catch (ArithmeticException e) {
             System.out.println("Catching exception: " + e);
         }
-
     }
 }
